41c9dba6be49ef861261b12776378a7e05049a46
Nick Reuter <nreuter85@gmail.com>
2017-09-03 06:06:10 -0500

    soong: handle qcom audio/display/media paths
    Change-Id: Iead25f930b8bca26d31450f9709a44a8c23b3031

--- android/variable.go
+++ android/variable.go
@@ -169,6 +169,11 @@ type productVariables struct {
 	Override_rs_driver *string `json:",omitempty"`
 	BoardUsesQTIHardware *bool `json:",omitempty"`
 
+	// Pathmap variables
+	QTIAudioPath *string `json:",omitempty"`
+	QTIDisplayPath *string `json:",omitempty"`
+	QTIMediaPath *string `json:",omitempty"`
+
 	// include aosip variables
 	*android.ProductVariables
 }
